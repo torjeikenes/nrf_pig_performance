51fb162fc1aaf31c02c84ce11ed8e746b7367034
torjeikenes <torje.n.eikenes@gmail.com>
2023-03-22 14:30:10 +0100

    cleanup

--- pig_peripheral/main.c
+++ pig_peripheral/main.c
@@ -481,82 +481,6 @@ void bsp_event_handler(bsp_event_t event)
     }
 }
 
-/**@brief   Function for handling app_uart events.
- *
- * @details This function will receive a single character from the app_uart module and append it to
- *          a string. The string will be be sent over BLE when the last character received was a
- *          'new line' i.e '\r\n' (hex 0x0D) or if the string has reached a length of
- *          @ref NUS_MAX_DATA_LENGTH.
- */
-/**@snippet [Handling the data received over UART] */
-/*
-void uart_event_handle(app_uart_evt_t *p_event)
-{
-    static uint8_t data_array[BLE_NUS_MAX_DATA_LEN];
-    static uint8_t index = 0;
-    uint32_t err_code;
-
-    switch (p_event->evt_type)
-    {
-    case APP_UART_DATA_READY:
-        UNUSED_VARIABLE(app_uart_get(&data_array[index]));
-        index++;
-
-        if ((data_array[index - 1] == '\n') || (index >= (BLE_NUS_MAX_DATA_LEN)))
-        {
-            err_code = ble_nus_string_send(&m_nus, data_array, index);
-            if (err_code != NRF_ERROR_INVALID_STATE)
-            {
-                APP_ERROR_CHECK(err_code);
-            }
-
-            index = 0;
-        }
-        break;
-
-    case APP_UART_COMMUNICATION_ERROR:
-        APP_ERROR_HANDLER(p_event->data.error_communication);
-        break;
-
-    case APP_UART_FIFO_ERROR:
-        APP_ERROR_HANDLER(p_event->data.error_code);
-        break;
-
-    default:
-        break;
-    }
-}
-*/
-/**@snippet [Handling the data received over UART] */
-
-/**@brief  Function for initializing the UART module.
- */
-/**@snippet [UART Initialization] */
-/*
-static void uart_init(void)
-{
-    uint32_t err_code;
-    const app_uart_comm_params_t comm_params =
-        {
-            RX_PIN_NUMBER,
-            TX_PIN_NUMBER,
-            RTS_PIN_NUMBER,
-            CTS_PIN_NUMBER,
-            APP_UART_FLOW_CONTROL_DISABLED,
-            false,
-            UART_BAUDRATE_BAUDRATE_Baud115200};
-
-    APP_UART_FIFO_INIT(&comm_params,
-                       UART_RX_BUF_SIZE,
-                       UART_TX_BUF_SIZE,
-                       uart_event_handle,
-                       APP_IRQ_PRIORITY_LOWEST,
-                       err_code);
-    APP_ERROR_CHECK(err_code);
-}
-*/
-/**@snippet [UART Initialization] */
-
 /**@brief Function for initializing the Advertising functionality.
  */
 static void advertising_init(void)
@@ -716,7 +640,6 @@ int main(void)
         {
             if (mpu6050_register_read(0x3A, &status, 1))
             {
-                // NRF_LOG_WARNING("MPU6050: No status reading\r\n");
                 NRF_LOG_INFO("Interrupt status: %x\r\n", status);
             }
 
@@ -724,7 +647,6 @@ int main(void)
             {
                 acc_sum = abs(acc_value[0]) + abs(acc_value[1]) + abs(acc_value[2]) - idle_acceleration_sum;
                 total_acc_sum += abs(acc_sum);
-                // acc_values[acc_index] = total_acc_sum;
                 acc_index++;
                 NRF_LOG_INFO("ACC Sum:%d, total: %d, ACC index: %d\r\n", acc_sum, total_acc_sum, acc_index); // display the read values
 
@@ -739,7 +661,6 @@ int main(void)
                         APP_ERROR_CHECK(err_code);
                     }
                     NRF_LOG_INFO("Data sent\r\n"); // display the read values
-                    // NRF_LOG_INFO("ACC Values:%d, %d, %d\r\n", acc_value[0], acc_value[1], acc_value[2]); // display the read values
                     total_acc_sum = 0;
                     acc_index = 0;
                 }
